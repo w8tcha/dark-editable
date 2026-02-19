4df518770a75db630e33bb817c9e4c28e6fedecc
Ingo <thewatcher@watchersnet.de>
2026-02-19 09:48:34 +0100

    Fix disabling #26

--- example/index.js
+++
@@ -1,9 +0,0 @@
-import DarkEditable from '../dist/dark-editable';
-
-const editable = document.querySelectorAll('.dark-edit');
-let i = 0;
-const edit_length = editable.length;
-
-for (i = 0; i < edit_length; i++) {
-	new DarkEditable(editable[i], { pos: 'bottom' });
-}
